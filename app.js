a110df099b8a81147885517f8dbecf058885374f
petrubear <edison.martinez@gmail.com>
2021-05-24 12:17:08 -0500

    feat: add yargs

--- app.js
+++ app.js
@@ -1,13 +1,55 @@
-const validator = require('validator')
-const getNotes = require('./notes.js')
-const chalk = require('chalk');
-const log = console.log;
+const chalk = require('chalk')
+const yargs = require('yargs')
+const log = console.log
 
-const msg = getNotes()
-console.log(msg)
+//customize yargs version
+yargs.version('0.0.1')
 
-console.log(validator.isEmail('test@test.com'))
-console.log(validator.isURL('http://test'))
+//yargs commands
+yargs.command({
+    command: 'add',
+    describe: 'add new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        log('Title: ', argv.title)
+        log('Body: ', argv.body)
+        log(chalk.green('note added'))
+    }
+})
 
-log(chalk.red.bold('Error'))
-log(chalk.green.bold('Success'))
+yargs.command({
+    command: 'remove',
+    describe: 'removes a note',
+    handler: function () {
+        log(chalk.green('removing a note'))
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'list existing notes',
+    handler: function () {
+        log(chalk.green('listing notes'))
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'reads a note',
+    handler: function () {
+        log(chalk.green('reading a note'))
+    }
+})
+
+yargs.parse()
