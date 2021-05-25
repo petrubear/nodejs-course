d473ece7593ad7d783d9e95be2dc4509ce1f803f
petrubear <edison.martinez@gmail.com>
2021-05-25 18:01:38 -0500

    feat: add playground files

---
+++ playground/2-arrow-function.js
@@ -0,0 +1,29 @@
+const log = console.log
+
+const square = function (x) {
+    return x * x
+}
+
+const squareArrow = (x) => {
+    return x * x
+}
+
+const squareShort = (x) => x * x
+
+log(square(3))
+log(squareArrow(3))
+log(squareShort(3))
+
+const event = {
+    name: 'Party',
+    //printGuestList: () => log('Guest List for:' + this.name) // arrow functions no pueden acceder a this!
+    guestList: ['Mayra', 'Lore', 'Kari'],
+    printGuestList () {
+        log('Guest list for ' + this.name)
+        this.guestList.forEach((guest) => {
+            log(guest + ' is attending ' + this.name)
+        })
+    }
+}
+
+event.printGuestList()
---
+++ playground/json.js
@@ -0,0 +1,28 @@
+const fs = require('fs')
+
+const book = {
+    title: 'Ego is the enemy',
+    author:'Ryan Holiday'
+}
+
+const bookJson = JSON.stringify(book)
+console.log(bookJson)
+
+const parsedData = JSON.parse(bookJson)
+console.log(parsedData.title)
+
+const fileName = '1-json.json'
+fs.writeFileSync(fileName, bookJson)
+const dataBuffer = fs.readFileSync(fileName)
+const dataJson = dataBuffer.toString()
+const data = JSON.parse(dataJson)
+console.log(data.author)
+
+const challengeFile = 'data.json'
+const challengeBuffer = fs.readFileSync(challengeFile)
+const challengeJson = challengeBuffer.toString()
+const challengeData = JSON.parse(challengeJson)
+challengeData.name = 'Edison'
+challengeData.age = 39
+modifiedJson = JSON.stringify(challengeData)
+fs.writeFileSync(challengeFile, modifiedJson)
