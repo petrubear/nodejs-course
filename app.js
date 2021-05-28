09d2993395a4dbade21fdcda207b1cb9e4b09b53
petrubear <edison.martinez@gmail.com>
2021-05-27 19:27:14 -0500

    feat: invoke weather service

--- app.js
+++ app.js
@@ -2,8 +2,12 @@
 const log = console.log
 const request = require('postman-request')
 
-const url = "http://api.weatherstack.com/current?access_key=eb1cdbf17f681c6c8adb31928e8f4a63&query=Quit"
-request({url: url}, (error, response) => {
-    const data = JSON.parse(response.body)
-    log(data.current)
+const url = "http://api.weatherstack.com/current?access_key=eb1cdbf17f681c6c8adb31928e8f4a63&units=m&query=Quito"
+request({url: url, json: true}, (error, response) => {
+    const weather = response.body.current
+    const message = weather.weather_descriptions[0]
+        + ". It is currently " + weather.temperature
+        + " degrees out. There's a UVIndex of " + weather.uv_index
+        + " it feels like " + weather.feelslike + " degrees out."
+    log(message)
 })
