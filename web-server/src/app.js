c594deb6d6c4492d4ff2cb866e6e84fc6c9b59d7
petrubear <edison.martinez@gmail.com>
2021-06-02 20:59:35 -0500

    feat: add sample server responses

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,12 +3,37 @@ const express = require('express');
 const app = express();
 
 // app.com
-// app.com/help
-// app.com/about
 app.get('', (req, res) => {
-    res.send('Hello express');
+    res.send('<h1>Weather</h1>');
 });
 
+// app.com/help
+app.get('/help', (req, res) => {
+    res.send([{
+        name: 'Edison',
+        age: 39,
+    },
+    {
+        name: 'Sarah',
+        age: 39,
+    },
+    ]);
+});
+
+// app.com/about
+app.get('/about', (req, res) => {
+    res.send('<h1>About page</h1>');
+});
+
+// app.com/weather
+app.get('/weather', (req, res) => {
+    res.send({
+        forecast: 'Nublado, 30% posibilidad de lluvia',
+        location: 'Quito, Ecuador',
+    });
+});
+
+// Server config
 app.listen(3000, () => {
     console.log('Server is listening on 3000');
 });
