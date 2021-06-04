834da33876737bf9f2901223fe2b878e94db15c6
petrubear <edison.martinez@gmail.com>
2021-06-03 19:41:01 -0500

    refactor: static pages to hbs templates

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,9 +2,40 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
-// ruta para recursos estaticos
+
+// ruta para recursos estaticos y hbs templates
 const staticFolder = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates');
+
+// hbs configuration
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+
+// setup static folder
 app.use(express.static(staticFolder));
+
+app.get('', (req, res) => {
+    // template, objeto de valores
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Edison',
+    });
+});
+
+app.get('/about', (req, res) => {
+    // template, objeto de valores
+    res.render('about', {
+        title: 'About Me',
+        name: 'Edison',
+    });
+});
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        helpText: 'Get some help',
+    });
+});
 
 // app.com/weather
 app.get('/weather', (req, res) => {
