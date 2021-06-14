c2002dfbb5ef05ad461c0a4a0f7b5f6b7fe1bd0a
petrubear <edison.martinez@gmail.com>
2021-06-14 11:27:00 -0500

    feat: fetch example

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,18 @@
 console.log('client side js file is loaded');
+
+fetch('http://puzzle.mead.io/puzzle').then((response) => {
+    response.json().then((data) => {
+        console.log(data);
+    });
+});
+
+fetch('http://localhost:3000/weather?address=boston').then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            console.error.log(data.error);
+        } else {
+            console.log(data.location);
+            console.log(data.forecast);
+        }
+    });
+});
