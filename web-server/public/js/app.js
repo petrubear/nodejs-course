bc50738d2b698e0c8b3cd850b3dd3668c44b5a10
petrubear <edison.martinez@gmail.com>
2021-06-14 12:49:44 -0500

    fix: remove localhost

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -13,7 +13,7 @@ fetch('http://puzzle.mead.io/puzzle').then((response) => {
 */
 
 const fetchLocation = (location) => {
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 messageOne.textContent = data.error;
