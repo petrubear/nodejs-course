5e647df350d590b6eab83aa60c4f5ad0e59b3c54
petrubear <edison.martinez@gmail.com>
2021-06-14 12:13:16 -0500

    feat: html form for weather

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,18 +1,35 @@
 console.log('client side js file is loaded');
+const weatherForm = document.querySelector('form');
+const searchInput = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
 
+/*
 fetch('http://puzzle.mead.io/puzzle').then((response) => {
     response.json().then((data) => {
         console.log(data);
     });
 });
+*/
 
-fetch('http://localhost:3000/weather?address=boston').then((response) => {
-    response.json().then((data) => {
-        if (data.error) {
-            console.error.log(data.error);
-        } else {
-            console.log(data.location);
-            console.log(data.forecast);
-        }
+const fetchLocation = (location) => {
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error;
+            } else {
+                messageOne.textContent = data.location;
+                messageTwo.textContent = data.forecast;
+            }
+        });
     });
+};
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    messageOne.textContent = 'Loading...';
+    messageTwo.textContent = '';
+    const location = searchInput.value;
+    fetchLocation(location);
 });
